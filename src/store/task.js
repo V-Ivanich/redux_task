7c2d5622117c551682956b700c4a3bf417322997
V-Ivanich <chipai_dom@mail.ru>
2023-03-14 21:43:44 +0300

    add reducer

---
+++ src/store/task.js
@@ -0,0 +1,38 @@
+import { createAction, createReducer } from '@reduxjs/toolkit'
+
+const update = createAction('task/updated')
+const remove = createAction('task/removed')
+
+export function taskCompleted(id) {
+  return update({ id, completed: true })
+}
+
+export function titleChanged(id) {
+  return update({ id, title: `New title for ${id}` })
+}
+
+export function taskDeleted(id) {
+  return remove({ id })
+}
+
+function taskReducer(state = [], action) {
+  switch (action.type) {
+    case update.type:
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id,
+      )
+      newArray[elementIndex] = {
+        ...newArray[elementIndex],
+        ...action.payload,
+      }
+      return newArray
+
+    case remove.type:
+      return state.filter((el) => el.id !== action.payload.id)
+
+    default:
+      return state
+  }
+}
+export default taskReducer
