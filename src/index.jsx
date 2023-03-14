42e93428df3cc6e2bca9f409e9f8374cd1608c89
V-Ivanich <chipai_dom@mail.ru>
2023-03-14 19:56:02 +0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import * as actions from './store/actionTypes'
+import { initiateStore } from './store/store'
+
+const store = initiateStore()
+
+const App = (params) => {
+  const [state, setState] = useState(store.getState())
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState())
+    })
+  }, [])
+
+  const completeTask = (taskId) => {
+    store.dispatch({
+      type: actions.taskUpdated,
+      payload: { id: taskId, completed: true },
+    })
+  }
+
+  const changeTitle = (taskId) => {
+    store.dispatch({
+      type: actions.taskUpdated,
+      payload: { id: taskId, title: `New title for ${taskId}` },
+    })
+  }
+
+  return (
+    <>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Complete</button>
+            <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
